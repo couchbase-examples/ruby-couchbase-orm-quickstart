f461e6b7d4d894ce691df6ed45da9bfeca55ee74
teetangh <teetangh@gmail.com>
2024-04-18 14:02:04 +0530

    added initial files

---
+++ app/models/airline.rb
@@ -0,0 +1,15 @@
+require 'couchbase-orm'
+
+class Airline < ApplicationRecord
+  attribute :name, :string
+  attribute :callsign, :string
+  attribute :iata, :string
+  attribute :icao, :string
+  attribute :country, :string
+
+  validates :name, presence: true
+  validates :callsign, presence: true
+  validates :iata, presence: true, length: { is: 2 }
+  validates :icao, presence: true, length: { is: 3 }
+  validates :country, presence: true
+end
