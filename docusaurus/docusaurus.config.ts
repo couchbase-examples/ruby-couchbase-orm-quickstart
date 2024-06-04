ccd772bdcea440f0dd28f5f9148662d055cf1627
Kaustav Ghosh <teetangh@gmail.com>
2024-06-04 12:11:30 +0530

    chore: Update Docusaurus configuration for Ruby Couchbase ORM tutorial

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://couchbase-examples.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ruby-couchbase-orm-quickstart/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
